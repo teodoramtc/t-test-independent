622e73a001507cb8c87f2679c9976b3873bf82ff
TeodoraM <teodoramtc@gmail.com>
2021-09-27 15:09:17 +0200

    stepsize changed

--- t-test-ind.py
+++ t-test-ind.py
@@ -4,10 +4,10 @@ import streamlit as st
 
 st.title("Independent samples t-test calculator")
 
-m1 = st.number_input("Mean value of the first group:", min_value=float(-10000000000), max_value=float(10000000000), value=float(0), step=float(1))
-m2 = st.number_input("Mean value of the second group:", min_value=float(-10000000000), max_value=float(10000000000), value=float(0), step=float(1))
-s1 = st.number_input("Standard deviation of the first group:", min_value=0.00000000000001, max_value=float(10000000000), value=float(1), step=float(1))
-s2 = st.number_input("Standard deviation of the second group:", min_value=0.00000000000001, max_value=float(10000000000), value=float(1), step=float(1))
+m1 = st.number_input("Mean value of the first group:", min_value=float(-10000000000), max_value=float(10000000000), value=float(0), step=float(0.01))
+m2 = st.number_input("Mean value of the second group:", min_value=float(-10000000000), max_value=float(10000000000), value=float(0), step=float(0.01))
+s1 = st.number_input("Standard deviation of the first group:", min_value=0.00000000000001, max_value=float(10000000000), value=float(1), step=float(0.01))
+s2 = st.number_input("Standard deviation of the second group:", min_value=0.00000000000001, max_value=float(10000000000), value=float(1), step=float(0.01))
 n1 = st.number_input("Size of the first group:", min_value=float(1), max_value=float(10000000000), value=float(100), step=float(1))
 n2 = st.number_input("Size of the second group:", min_value=float(1), max_value=float(10000000000), value=float(100), step=float(1))
 
